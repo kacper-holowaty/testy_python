d2177f2b797049d32e5541b8c5ffb9e64e43b070
kacper-holowaty <kacper.holowaty@gmail.com>
2022-01-24 23:58:31 +0100

    dodawanie funkcja

---
+++ dodawanie.py
@@ -0,0 +1,7 @@
+a = 10
+b = 17 
+
+def dodawanie(a,b):
+	return a + b
+print(dodawanie(a,b))
+
---
+++ test_dodawanie.py
@@ -0,0 +1,6 @@
+import testy_python from dodawanie
+
+def test_dodawanie():
+	a = 14
+	b = 20
+	assert dodawanie(a,b) == a+b
