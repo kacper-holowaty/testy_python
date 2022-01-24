cd8fe7748e0f5f5a172df84b94726160cb9913ca
kacper-holowaty <kacper.holowaty@gmail.com>
2022-01-25 00:16:50 +0100

    zadziałaj...

--- funkcja.py
+++ funkcja.py
@@ -5,9 +5,9 @@ def sortowanie(owoce):
 		j = 0
 		while j < len(owoce)-1:
 			if owoce[j] > owoce[j+1]:
-				tmp = lista[j]
-				lista[j] = lista[j+1]
-				lista[j+1] = tmp
+				tmp = owoce[j]
+				owoce[j] = owoce[j+1]
+				owoce[j+1] = tmp
 			j += 1
 		i += 1
-	return lista
+	return owoce
